bfac84cd916a7f415622be06d6df6e36a47f27e7
owend <owendane5@gmail.com>
2024-03-06 11:33:43 -0500

    first commit

---
+++ js/hotwheels.js
@@ -0,0 +1,23 @@
+const output = document.getElementById('output');
+
+function Hotwheels(color, decal, productManufacturer, vehicleManufacturer, modelName, packageColor, productNumber, seriesName, seriesNumber, rims, rimColor, rimSize,) {
+this.color = color;
+this.decal = decal;
+this.productManufacturer = productManufacturer;
+this.vehicleManufacturer = vehicleManufacturer;
+this.modelName = modelName;
+this.packageColor = packageColor;
+this.productNumber = productNumber;
+this.seriesName = seriesName;
+this.seriesNumber = seriesNumber;
+this.describe(){
+    let description = `The color is ${this.color} it doe`
+}
+
+}
+
+const hotWheels2 = new Hotwheels("")
+
+const hotWheels3 = new Hotwheels("")
+
+// output.textContent = 
